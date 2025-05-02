add43a797f03e2a920d746142ccdf9331e789cae
Ian kiprotich <113979508+onai254@users.noreply.github.com>
2025-05-02 22:19:10 +0300

    Update kubelet.sh

--- Scripts/kubelet.sh
+++ Scripts/kubelet.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+sudo apt-get update
 
-# Add the Kubernetes repository
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-# Update the package list
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update
 
 # Install Kubernetes, kubeadm, kubelet, and kubectl
@@ -15,3 +16,5 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 # Start kubelet
 sudo systemctl start kubelet
+
+sudo systemctl enable --now kubelet
